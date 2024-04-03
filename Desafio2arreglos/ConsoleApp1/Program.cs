574658f6b73e086937ae2ba0662442975fc44208
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2024-04-02 20:10:31 -0600

    Desafio arreglos en C#

---
+++ Desafio2arreglos/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            int[,] numeros = new int[5, 5];
+
+            CalculoCompras(numeros);
+
+            Console.ReadKey(true);
+        }
+
+        public static void CalculoCompras(int[,] listaCompras)
+        {
+            int[] precioFinalClientes = new int[listaCompras.GetLength(0)];
+
+            for (int i = 0; i < listaCompras.GetLength(0); i++)
+            {
+                for (int j = 0; j < listaCompras.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Ingresa el precio del cliente {i +1} No {j}: ");
+
+                    listaCompras[i, j] = int.Parse(Console.ReadLine());
+                    precioFinalClientes[i] += listaCompras[i, j];
+
+                    
+                }
+            }
+
+            for ( int i = 0; i < precioFinalClientes.Length; i++)
+            {
+                if (precioFinalClientes[i] <= 99)
+                {
+                    Console.WriteLine($"cliente {i+1} no tiene desucento");
+                }
+                else if (precioFinalClientes[i] >= 100 && precioFinalClientes[i] <= 1000) 
+                {
+                    Console.WriteLine($" cliente {i + 1} si tiene un descuento del 10% que tendra un precio final de: { precioFinalClientes[i] - (precioFinalClientes[i] * 0.1) }");
+                }
+
+                else if (precioFinalClientes[i] >=1000)
+                {
+                    Console.WriteLine($" cliente {i+1} si aplica el descuento del 20% que tendra un precio final de: { precioFinalClientes[i] - (precioFinalClientes[i] * 0.2) }");
+
+                }
+                        }
+                
+        }
+    }
+}
---
+++ Desafio3arreglos/Desafio3arreglos/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3arreglos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool validacion = true;
+            int opcion;
+            List<string> lista = new List<string>(){};
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Menu de opciones");
+                Console.WriteLine("1. Mostrar");
+                Console.WriteLine("2. Agregar");
+                Console.WriteLine("3. Eliminar");
+                Console.WriteLine("4. Salir");
+
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion) {
+                    case 1:
+
+                        foreach (string elemento in lista)
+                        {
+                            Console.WriteLine(elemento);
+                        }
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Escriba la tarea que desea agregar:");
+                        string tareaPorAgregar = Console.ReadLine();
+                        lista.Add(tareaPorAgregar);
+                        break;
+
+
+                        case 3:
+                        Console.WriteLine("Escribe la tarea que deseas eliminar: ");
+                        string tareaPorEliminar = Console.ReadLine();
+
+                        lista.Remove(tareaPorEliminar);
+
+                        break;
+
+                    case 4: validacion = false; break;
+                }
+                Console.ReadKey(true);
+
+            } while (validacion);
+        }
+    }
+}
